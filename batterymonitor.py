3403da48fade1de761633adfd2dc5c72e6d40c61
Takao Akaki <mongonta@gmail.com>
2019-07-27 17:07:42 +0900

    Update batterymonitor.py
    add filler

--- batterymonitor.py
+++ batterymonitor.py
@@ -2,9 +2,8 @@ import lcd
 import utime
 import sys
 import pmu
-from machine import I2C
-from Maix import GPIO
-from fpioa_manager import *
+
+filler = "          "
 
 axp = pmu.axp192()
 
@@ -17,22 +16,22 @@ lcd.draw_string(230, 0, "*", lcd.BLUE, lcd.BLACK)
 try:
   while(True):
     val = axp.getVbatVoltage()
-    lcd.draw_string(0, 15, "Battery Voltage:" + str(val), lcd.RED, lcd.BLACK)
+    lcd.draw_string(0, 15, "Battery Voltage:" + str(val) + filler, lcd.RED, lcd.BLACK)
 
     val = axp.getUSBVoltage()
-    lcd.draw_string(0, 30, "USB Voltage:" + str(val), lcd.WHITE, lcd.BLACK)
+    lcd.draw_string(0, 30, "USB Voltage:" + str(val) + filler, lcd.WHITE, lcd.BLACK)
 
     val = axp.getUSBInputCurrent()
-    lcd.draw_string(0, 45, "USB InputCurrent:" + str(val), lcd.RED, lcd.BLACK)
+    lcd.draw_string(0, 45, "USB InputCurrent:" + str(val) + filler, lcd.RED, lcd.BLACK)
 
     val = axp.getBatteryDischargeCurrent()
-    lcd.draw_string(0, 60, "DischargeCurrent:" + str(val), lcd.GREEN, lcd.BLACK)
+    lcd.draw_string(0, 60, "DischargeCurrent:" + str(val) + filler, lcd.GREEN, lcd.BLACK)
 
     val = axp.getBatteryInstantWatts()
-    lcd.draw_string(0, 75, "Instant Watts:" + str(val), lcd.BLUE, lcd.BLACK)
+    lcd.draw_string(0, 75, "Instant Watts:" + str(val) + filler, lcd.BLUE, lcd.BLACK)
 
     val = axp.getTemperature()
-    lcd.draw_string(0, 90, "Temperature:" + str(val), lcd.BLUE, lcd.BLACK)
+    lcd.draw_string(0, 90, "Temperature:" + str(val) + filler, lcd.BLUE, lcd.BLACK)
 
     utime.sleep(1)
 
